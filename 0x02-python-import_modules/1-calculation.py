81d401658d5beef836a6240c4ca6c765420debd1
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-26 14:36:22 +0300

    added a calculator file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ import calculator_1
 a = 10
 b = 5
 
-sum_ab = calculator_1.add(a, b)
-diff_ab = calculator_1.sub(a, b)
-product_ab = calculator_1.mul(a, b)
-quotient_ab = calculator_1.div(a, b)
-
-print(f"{a} + {b} = {sum_ab}")
-print(f"{a} - {b} = {diff_ab}")
-print(f"{a} * {b} = {product_ab}")
-print(f"{a} / {b} = {quotient_ab}")
+print(f"{a} + {b} = {calculator_1.add(a, b)}")
+print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+print(f"{a} / {b} = {calculator_1.div(a, b)}")
