88d67502750d6c8c3e60e21e82356736b91025b0
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-26 14:39:06 +0300

    added a calculator file A

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,3 +9,6 @@ print(f"{a} + {b} = {calculator_1.add(a, b)}")
 print(f"{a} - {b} = {calculator_1.sub(a, b)}")
 print(f"{a} * {b} = {calculator_1.mul(a, b)}")
 print(f"{a} / {b} = {calculator_1.div(a, b)}")
+
+if __name__ == "__main__":
+    pass
