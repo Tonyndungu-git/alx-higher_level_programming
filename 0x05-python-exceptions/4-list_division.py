2c994c67ce3ae04940abff59620bd1d344c914df
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-03-27 19:48:57 +0300

    troubleshooting list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
-    list = []
-    count = 0
+    result = []
+    shorter_length = min(len(my_list_1), len(my_list_2), list_length)
 
-    for i in range(list_length):
+    for i in range(shorter_length):
         try:
-            if isinstance(my_list_1[i], int) and isinstance(my_list_2[i], int):
-                try:
-                    list.append(my_list_1[i] / my_list_2[i])
-                except ZeroDivisionError:
-                    print("division by 0")
-            else:
-                raise TypeError
+            if not isinstance(my_list_1[i], int) or not isinstance(my_list_2[i], int):
+                raise TypeError("wrong type")
+            if my_list_2[i] == 0:
+                raise ZeroDivisionError("division by 0")
+            result.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError as e:
+            print(e)
+            result.append(0)
+        except TypeError as e:
+            print(e)
+            result.append(0)
         except IndexError:
             print("out of range")
-            break
-        except TypeError:
-            print("wrong type")
-        else:
-            count += 1
+            result.append(0)
+        finally:
+            pass
 
-    return list
+    # Add zeros for remaining elements in longer list(s)
+    result += [0] * (list_length - shorter_length)
+
+    return result
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
+
+print("--")
+
+my_l_1 = [10, 8, 4, 4]
+my_l_2 = [2, 0, "H", 2, 7]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
