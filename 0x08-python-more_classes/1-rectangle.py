9865c4e9f8b043455b9082dc02549a8998dc215b
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-05 16:25:56 +0300

    defining widh and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+'''module class'''
+
 
 class Rectangle:
+    '''initialization'''
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-
+        '''width getter '''
         @property
         def width(self):
             return self.__width
 
-
+        '''width setter'''
         @width.setter
         def width(self, value):
             if not isinstance(value, int):
@@ -20,12 +23,12 @@ class Rectangle:
             else:
                 self.__width = width
 
-
+        '''height getter'''
         @property
         def height(self, value):
             return self.__width
 
-
+        '''height setter'''
         @height.setter
         def height(self, value):
             if not isinstance(value, int):
