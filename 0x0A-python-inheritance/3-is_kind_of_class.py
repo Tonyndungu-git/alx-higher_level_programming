f5db20225ecc07094dad023e4c2e429e7a4438bf
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-11 19:06:01 +0300

    corrections 0f is same

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,4 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     '''function that returns True if the object is an instance of a class'''
-    isinstance(obj, a_class):
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
