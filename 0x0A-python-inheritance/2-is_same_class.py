ab002e6673b08f19e94b8feabc2069323ba6ad40
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-11 19:03:58 +0300

    corrections 0f is same

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' is_same_class '''
 
+
 def is_same_class(obj, a_class):
     ''' function that compares if an object is the instance of a class'''
     return type(obj) is a_class
