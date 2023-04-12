4cff5e8ac4c6a7368b21ea78317b9ee65ca2400c
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-04-12 12:21:22 +0300

    function that reads a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     ''' reads a text file '''
     with open(filename, 'r', encoding='utf8') as f:
-        text = f.read()
-    print(text)
+        print(f.read(), end='')
