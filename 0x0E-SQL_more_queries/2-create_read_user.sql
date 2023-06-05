eefa02a1b1c1da77f969d014d01b2b2e573e768c
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-05 14:35:59 +0300

    create user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Create A USER
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS
+user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON
+*.* TO user_0d_1@localhost;
