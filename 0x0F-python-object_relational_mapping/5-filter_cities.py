971b6e1db90c1d77af9cd98393b816d74844649a
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-19 17:44:06 +0300

    question 5 correction

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
-''' '''
+''' search for citities in a certain state'''
 
 import sys
 import MySQLdb
 
-def get_cities_of_state(username, password, database, state_name):
-    ''' Connect to MySQL server '''
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db.cursor()
 
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
-
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    state_name = sys.argv[4]
+    cursor = conn.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (state_name,))
     rows = cursor.fetchall()
-
+    re = ""
     for row in rows:
-        print(f"{row[1]}, ", end="")
-    print ("\n")
-    db.close()
+        re += row[0] + ", "
+    print(re[0:-2:])
+    cursor.close()
+    conn.close()
+
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    get_cities_of_state(username, password, database, state_name)
+    main()
