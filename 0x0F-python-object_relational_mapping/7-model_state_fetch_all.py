c5647353e7c4f84233abe73c02544279c6b74e62
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-25 15:21:33 +0300

    correction 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,29 +1,24 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects
+from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
 
-def list_states(username, password, database):
-    '''Configure the connection URL'''
-    connection_url = f'mysql://{username}:{password}@localhost:3306/{database}'
-
-    engine = create_engine(connection_url)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    session.close()
-
-
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
