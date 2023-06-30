15f61c8d0a1bbde40f11489640b5fc70bfafeaaa
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-30 14:00:01 +0300

    corrections for script 0 and 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
-import urllib.request
+""" Python script that fetches https://intranet.hbtn.io/status """
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    url = "https://alx-intranet.hbtn.io/status"
 
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body))
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
-import urllib.request
-import sys
+"""script that sends a request to the URL and displays the value of
+the X-Request-Id variable found in the header of the response """
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-with urllib.request.urlopen(url) as response:
-    header = response.getheader('X-Request-Id')
+    url = sys.argv[1]
 
-print(header)
+    with urllib.request.urlopen(url) as response:
+        header = response.info()
+        print(header['X-Request-Id'])
