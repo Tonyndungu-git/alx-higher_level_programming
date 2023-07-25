7c0fdadf070544a1801fd7a77e8c32aaada63758
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-25 20:46:29 +0300

    correction script 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,15 @@
 const req = require('request');
 const apiUrl = process.argv[2];
 const characterId = 18;
+const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
 
 req.get(apiUrl, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     if (response.statusCode === 200) {
-      const films = JSON.parse(body).results;
-      const numberOfMovies = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+	const films = JSON.parse(body).results;
+	      const numberOfMovies = films.filter(film => film.characters.includes(characterUrl)).length;
       console.log(numberOfMovies);
     } else {
       console.error('Failed to fetch data:', response.statusCode);
