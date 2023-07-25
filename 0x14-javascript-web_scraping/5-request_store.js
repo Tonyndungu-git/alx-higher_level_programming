0c2ddddae2fa9a955cee583d5c77358eaa53428a
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-07-25 20:49:46 +0300

    correction script 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
 /* script that gets the contents of a webpage and stores it in a file */
-
 const request = require('request');
 const fs = require('fs');
-
 const url = process.argv[2];
 const filePath = process.argv[3];
 
